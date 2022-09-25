1dfc2493488215134652ae19150a687983355393
AbodeNK <abdulaa.mousa.cpu@gmail.com>
2022-09-25 14:34:00 +0200

    bericht

--- DEV/Angular/my-app/src/app/home/home.component.ts
+++ DEV/Angular/my-app/src/app/home/home.component.ts
@@ -7,10 +7,35 @@ import { AuthService } from '../Service/auth.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  bin: string ="";
+  dez: number;
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
-
+  dezZuBin(value) {
+    this.bin = "";
+    if(!value)
+    {
+      this.bin = "";
+      return;
+    }
+    if(value == 0)
+    {
+      this.bin +="0";
+    }
+    for(let i = value; i>=1;)
+    {
+      i = i / 2;
+      if(i === +i && i === (i|0))
+      {
+        this.bin = "0"+this.bin;
+      }
+      else if(i === +i && i !== (i|0))
+      { 
+        i = i-0.5 ;
+        this.bin =  "1"+this.bin;
+      }
+    }
+  }
 }
